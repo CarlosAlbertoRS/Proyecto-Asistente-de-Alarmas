e8d4bd696462dbcc9e2e92c5af2b9408f9c43024
CarlosAlbertoRS <rubiosoliscarlosalberto.125b@gmail.com>
2023-11-10 15:53:05 -0600

    Actualizacion test - 10/11/2023

--- app/src/main/java/com/example/proyasyst_test/testeo.kt
+++ app/src/main/java/com/example/proyasyst_test/testeo.kt
@@ -1,20 +1,20 @@
 package com.example.proyasyst_test
 
-import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.Switch
 
-class MainActivity : AppCompatActivity() {
+class testeo : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_testeo)
 
-        var bt1 = this.findViewById<Button>(R.id.btn_test)
+        var bt1 = findViewById<Button>(R.id.button)
+        var sw1 = findViewById<Switch>(R.id.sw_estado)
 
         bt1.setOnClickListener {
-            var intent = Intent(this, testeo::class.java)
-            startActivity(intent)
+            sw1.isEnabled = true;
         }
 
     }
