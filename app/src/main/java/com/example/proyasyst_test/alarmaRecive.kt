48f9124a1ae9acb383732e8ca98dc4add3aaf27b
CarlosAlbertoRS <rubiosoliscarlosalberto.125b@gmail.com>
2023-11-10 19:20:42 -0600

    ConfiguracionInicialAlarma

---
+++ app/src/main/java/com/example/proyasyst_test/alarmaRecive.kt
@@ -0,0 +1,11 @@
+package com.example.proyasyst_test
+
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+
+class alarmaRecive: BroadcastReceiver() {
+    override fun onReceive(context: Context?, p1: Intent?) {
+
+    }
+}
