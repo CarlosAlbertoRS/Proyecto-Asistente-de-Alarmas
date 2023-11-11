9b2d4bee1877ae3430e1b76815fcaf3d175bf8df
CarlosAlbertoRS <rubiosoliscarlosalberto.125b@gmail.com>
2023-11-10 21:21:32 -0600

    Final = 10/11/2023

--- app/src/main/java/com/example/proyasyst_test/alarmaRecive.kt
+++ app/src/main/java/com/example/proyasyst_test/alarmaRecive.kt
@@ -1,11 +1,33 @@
 package com.example.proyasyst_test
 
+import android.annotation.SuppressLint
+import android.app.PendingIntent
 import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
+import androidx.core.app.NotificationCompat
+import androidx.core.app.NotificationManagerCompat
 
 class alarmaRecive: BroadcastReceiver() {
-    override fun onReceive(context: Context?, p1: Intent?) {
+    @SuppressLint("MissingPermission")
+    override fun onReceive(context: Context?, intent: Intent?) {
+
+        val i = Intent(context, DestinationActivity::class.java)
+        intent!!.flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
+        val pendingIntent = PendingIntent.getActivities(context,0, arrayOf(i),
+            PendingIntent.FLAG_IMMUTABLE)
+
+        val builder = NotificationCompat.Builder(context!!,"Asys-T")
+            .setSmallIcon(R.drawable.ic_launcher_background)
+            .setContentTitle("Alarma -> Asys-T")
+            .setContentText("Hola amiguito, es la hora de tomarte tus pastillas...")
+            .setAutoCancel(true)
+            .setDefaults(NotificationCompat.DEFAULT_ALL)
+            .setPriority(NotificationCompat.PRIORITY_HIGH)
+            .setContentIntent(pendingIntent)
+
+        val notificationManager = NotificationManagerCompat.from(context)
+        notificationManager.notify(123,builder.build())
 
     }
 }
--- app/src/main/java/com/example/proyasyst_test/testeo.kt
+++ app/src/main/java/com/example/proyasyst_test/testeo.kt
@@ -1,21 +1,118 @@
 package com.example.proyasyst_test
 
+import android.app.AlarmManager
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.app.PendingIntent
+import android.content.Intent
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import android.widget.Switch
+import android.widget.Toast
+import com.example.proyasyst_test.databinding.ActivityMainBinding
+import com.google.android.material.timepicker.MaterialTimePicker
+import com.google.android.material.timepicker.TimeFormat
+import java.util.Calendar
 
 class testeo : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var picker: MaterialTimePicker
+    private lateinit var calendar: Calendar
+    private lateinit var alarmManager: AlarmManager
+    private lateinit var pendingIntent: PendingIntent
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_testeo)
 
         var bt1 = findViewById<Button>(R.id.button)
-        var sw1 = findViewById<Switch>(R.id.sw_estado)
+        var bt2 = findViewById<Button>(R.id.Cancelar)
+        var bt3 = findViewById<Button>(R.id.Seleccionar)
 
+        var sw1 = findViewById<Switch>(R.id.sw_estado)
+        createNotificationChannel()
         bt1.setOnClickListener {
-            sw1.isEnabled = true;
+            setAlarm()
+        }
+        bt2.setOnClickListener {
+            cancelAlarm()
+        }
+        bt3.setOnClickListener {
+            setPicker()
         }
 
+    }
+
+    private fun setPicker() {
+        picker = MaterialTimePicker.Builder()
+            .setTimeFormat(TimeFormat.CLOCK_12H)
+            .setHour(12)
+            .setMinute(0)
+            .setTitleText("Selecto Besto Meme")
+            .build()
+
+        picker.show(supportFragmentManager,"Asys-T")
+
+        picker.addOnPositiveButtonClickListener {
+            if(picker.hour > 12){
+                String.format("%02d",picker.hour - 12) + " : " + String.format(
+                    "%02d", picker.minute
+                ) + "PM"
+            } else{
+                String.format("%02d",picker.hour) + " : " + String.format(
+                    "%02d", picker.minute
+                ) + "AM"
+            }
+
+            calendar = Calendar.getInstance()
+            calendar[Calendar.HOUR_OF_DAY] = picker.hour
+            calendar[Calendar.MINUTE] = picker.minute
+            calendar[Calendar.SECOND] = 0
+            calendar[Calendar.MILLISECOND] = 0
+        }
+    }
+
+    private fun cancelAlarm() {
+        alarmManager = getSystemService(ALARM_SERVICE) as AlarmManager
+        val intent = Intent(this,alarmaRecive::class.java)
+
+        pendingIntent = PendingIntent.getBroadcast(this,0,intent, PendingIntent.FLAG_IMMUTABLE)
+        alarmManager.cancel(pendingIntent)
+
+        Toast.makeText(this,"Cancelada",Toast.LENGTH_SHORT).show()
+    }
+
+    private fun setAlarm() {
+
+        alarmManager = getSystemService(ALARM_SERVICE) as AlarmManager
+        val intent = Intent(this,alarmaRecive::class.java)
+
+        pendingIntent = PendingIntent.getBroadcast(this,0,intent, PendingIntent.FLAG_IMMUTABLE)
+
+        alarmManager.setRepeating(
+            AlarmManager.RTC_WAKEUP,calendar.timeInMillis,
+            AlarmManager.INTERVAL_DAY,pendingIntent
+        )
+
+        Toast.makeText(this,"Listo",Toast.LENGTH_SHORT).show()
+
+    }
+
+    private fun createNotificationChannel() {
+        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            val name : CharSequence = "canalRecordatorio"
+            val description = "Channel for Alarm Manager"
+            val importance = NotificationManager.IMPORTANCE_HIGH
+            val channel = NotificationChannel("Asys-T",name,importance)
+            channel.description = description
+            val notificationManager = getSystemService(
+                NotificationManager::class.java
+            )
+
+            notificationManager.createNotificationChannel(channel)
+        }
     }
 }
