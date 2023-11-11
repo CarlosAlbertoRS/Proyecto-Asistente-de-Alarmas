91c9624744ca3971c130e7c527d4ccf7fad64b1e
Brendis26 <sotb010326@itsuruapan.edu.mx>
2023-11-10 21:04:32 -0600

    plantilla1

--- app/src/main/java/com/example/proyasyst_test/logo.kt
+++ app/src/main/java/com/example/proyasyst_test/logo.kt
@@ -3,9 +3,9 @@ package com.example.proyasyst_test
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class emergencia : AppCompatActivity() {
+class logo : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_emergencia)
+        setContentView(R.layout.activity_logo)
     }
 }
