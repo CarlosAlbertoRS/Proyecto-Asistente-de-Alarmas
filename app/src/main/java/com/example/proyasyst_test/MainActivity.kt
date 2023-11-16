c9164ccbddabbfd0c334228481ed1193075371f4
Brendis26 <sotb010326@itsuruapan.edu.mx>
2023-11-16 14:19:18 -0600

    avance1

--- app/src/main/java/com/example/proyasyst_test/MainActivity.kt
+++ app/src/main/java/com/example/proyasyst_test/MainActivity.kt
@@ -4,6 +4,7 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.ImageButton
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -15,6 +16,12 @@ class MainActivity : AppCompatActivity() {
         bt1.setOnClickListener {
             var intent = Intent(this, testeo::class.java)
             startActivity(intent)
+        }
+
+        val btnRed = this.findViewById<Button>(R.id.btnRed)
+
+        btnRed.setOnClickListener {
+            startActivity(Intent(this, menu_principal::class.java))
         }
 
     }
