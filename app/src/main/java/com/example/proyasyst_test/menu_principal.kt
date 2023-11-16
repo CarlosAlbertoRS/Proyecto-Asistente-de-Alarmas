d1564302a52cff0e227798b7d87ea1524892edfb
Brendis26 <sotb010326@itsuruapan.edu.mx>
2023-11-16 01:47:46 -0600

    parte5.5

--- app/src/main/java/com/example/proyasyst_test/menu_principal.kt
+++ app/src/main/java/com/example/proyasyst_test/menu_principal.kt
@@ -1,11 +1,25 @@
 package com.example.proyasyst_test
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ImageButton
 
 class menu_principal : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_menu_principal)
+
+        val btnAgregar = findViewById<ImageButton>(R.id.btnAgregar)
+        val btnRegistro = findViewById<ImageButton>(R.id.btnAgregar)
+
+        btnAgregar.setOnClickListener {
+            startActivity(Intent(this, agregar::class.java))
+        }
+
+        btnRegistro.setOnClickListener {
+            startActivity(Intent(this, MenuRegistro::class.java))
+        }
+
     }
 }
