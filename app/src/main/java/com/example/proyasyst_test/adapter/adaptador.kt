93fe913a7c0c0605de8a97bd4b5d6d52740ad6f0
Brendis26 <sotb010326@itsuruapan.edu.mx>
2023-12-06 16:16:57 -0600

    avance34.5

--- app/src/main/java/com/example/proyasyst_test/adapter/adaptador.kt
+++ app/src/main/java/com/example/proyasyst_test/adapter/adaptador.kt
@@ -8,32 +8,6 @@ import androidx.recyclerview.widget.RecyclerView
 import com.example.proyasyst_test.R
 import com.example.proyasyst_test.variables
 
-class adaptador
-    : RecyclerView.Adapter<adaptador.ViewHolder>() {
-        inner class ViewHolder: RecylerView.ViewHolder{
-
-            val txtMedic: EditText
-            val txtIntervalo: EditText
-            val  txtTotal: EditText
-            val txtEsado: EditText
-
-            constructor(view: View) : supes(view){
-
-            }
-
-
-        }
-
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
-        TODO("Not yet implemented")
-    }
-
-    override fun getItemCount(): Int {
-        TODO("Not yet implemented")
-    }
-
-    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
-        TODO("Not yet implemented")
-    }
+class adaptador{
 
 }
