49d91a90f5e6d3be0e1e9a2ca8d67a18450375d0
Brendis26 <sotb010326@itsuruapan.edu.mx>
2023-12-06 16:08:33 -0600

    avance34

--- app/src/main/java/com/example/proyasyst_test/adapter/adaptador.kt
+++ app/src/main/java/com/example/proyasyst_test/adapter/adaptador.kt
@@ -1,22 +1,39 @@
 package com.example.proyasyst_test.adapter
 
 import android.view.LayoutInflater
+import android.view.View
 import android.view.ViewGroup
+import android.widget.EditText
 import androidx.recyclerview.widget.RecyclerView
 import com.example.proyasyst_test.R
 import com.example.proyasyst_test.variables
 
-class adaptador(private val MedicinaLista: List<variables>, private val onClickListener: (variables)->Unit): RecyclerView.Adapter<viewFolder>() {
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): viewFolder {
-        val layoutInflater= LayoutInflater.from(parent.context)
-        return viewFolder(layoutInflater.inflate(R.layout.item_registro_activas,parent,false))
+class adaptador
+    : RecyclerView.Adapter<adaptador.ViewHolder>() {
+        inner class ViewHolder: RecylerView.ViewHolder{
+
+            val txtMedic: EditText
+            val txtIntervalo: EditText
+            val  txtTotal: EditText
+            val txtEsado: EditText
+
+            constructor(view: View) : supes(view){
+
+            }
+
+
+        }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        TODO("Not yet implemented")
     }
 
-    override fun onBindViewHolder(holder: viewFolder, position: Int) {
-        val item = MedicinaLista[position]
-        holder.render(item, onClickListener)
+    override fun getItemCount(): Int {
+        TODO("Not yet implemented")
     }
 
-    override fun getItemCount(): Int = MedicinaLista.size
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        TODO("Not yet implemented")
+    }
 
 }
